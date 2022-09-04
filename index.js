950ec285a863ecaaa1b043f4a8480cc136033386
NOVAHOrizonVelocity <77928053+meenrock@users.noreply.github.com>
2022-09-05 00:42:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const app = express();
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.applyMiddleware({app})
+
+app.listen({ port: 3001}, () => {
+    console.log("Server Started on PORT 3001")
+})
