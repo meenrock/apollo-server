7df393623fb09be4a63772b38da69cbf07b1d7b9
NOVAHOrizonVelocity <77928053+meenrock@users.noreply.github.com>
2022-09-04 23:09:31 +0700

    Initial commit

---
+++ userData.js
@@ -0,0 +1,24 @@
+let users = [
+    {
+        id: 1,
+        name: "meen",
+        status: true,
+    },
+    {
+        id: 2,
+        name: "mov",
+        status: false,
+    },
+    {
+        id: 3,
+        name: "movs",
+        status: false,
+    },
+    {
+        id: 4,
+        name: "mova",
+        status: false,
+    },
+
+]
+
