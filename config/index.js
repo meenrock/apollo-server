f79b1bfca808e09974a8a9d41d8f13e30b58103b
NOVAHOrizonVelocity <77928053+meenrock@users.noreply.github.com>
2022-09-30 23:13:32 +0700

    Create index.js

---
+++ config/index.js
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const port = process.env.PORT
+
+// You may use this as a boolean value for different situations
+const env = {
+  development: process.env.NODE_ENV === 'development',
+  test: process.env.NODE_ENV === 'test',
+  staging: process.env.NODE_ENV === 'staging',
+  production: process.env.NODE_ENV === 'production',
+}
+
+const dbConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  server: process.env.SERVER_NAME,
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+  options: {
+    encrypt: false,
+    trustServerCertificate: false, // change to true for local dev / self-signed certs
+  },
+}
+
+const awsConfig = {
+  S3Bucket: process.env.S3_BUCKET,
+  region: process.env.S3_REGION,
+}
+
+export { port, env, dbConfig, awsConfig }
